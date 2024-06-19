6eb905cf73bf7b22ad35d46c7932fdb087512049
Louise Lizé <louise.lize@icloud.com>
2024-06-19 20:24:15 +0200

    ✨ Get Cv from DB via Repository

--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CvEntity } from './entities/cv.entity/cv.entity';
 
 @Injectable()
-export class CvService {}
+export class CvService {
+    constructor(
+        @InjectRepository(CvEntity)
+        private cvRepository: Repository<CvEntity>
+    ){
+
+    }
+
+    async getCvs(): Promise<CvEntity[]>{
+        return await this.cvRepository.find();
+    }
+}
